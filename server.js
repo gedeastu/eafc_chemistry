82c27132082875e17660c2edb73fe89a9495bddc
Nama <achmaddani912@gmail.com>
2025-12-28 13:12:01 +0700

    Add validation input for terminal version

--- server.js
+++ server.js
@@ -8,141 +8,151 @@ import { preselectPlayers } from "./algorithms/preSelectPlayers.js";
 import * as readline from 'node:readline/promises';
 import { stdin as input, stdout as output } from 'node:process';
 
-const app = express();
-app.use(cors());
-app.use(express.json());
+// const app = express();
+// app.use(cors());
+// app.use(express.json());
 
-app.post("/api/optimize-squad", (req, res) => {
-    const { formation: key,jumlahPemainPerPosisi:countPlayer} = req.body;
-    const formation = formations[key];
-
-  if (!formation) {
-    return res.status(400).json({
-      error: "Formasi tidak valid"
-    });
-  }
-
-  const preselected = preselectPlayers(playersByPosition,formation.tactic,countPlayer);
-
-  // ===== ITERATIVE  =====
-const iterStart = performance.now();
-const itResult = backtrackingIterative(
-  formation.positions,
-  preselected
-);
-const iterEnd = performance.now();
-
-// ===== RECURSIVE =====
-const recStart = performance.now();
-const recResult = backtrackingRecursive(
-  formation.positions,
-  preselected
-);
-const recEnd = performance.now();
-
-  res.json({
-    formation: key,
-    jumlahPemainPerPosisi: countPlayer,
-    runtime: {
-    iterativeMs: Number((iterEnd - iterStart).toFixed(2)),
-    recursiveMs: Number((recEnd - recStart).toFixed(2))
-    },
-    preselectPlayers: {
-      total: Object.values(preselected).flat().length,
-      table: toTable(Object.values(preselected).flat())
-    },
-    iterativeResult: {
-      chemistry: itResult.score,
-      table: toTable(itResult.squad)
-    },
-    recursiveResult: {
-      chemistry: recResult.score,
-      table: toTable(recResult.squad)
-    }
-  });
-})
-app.listen(3000, () =>
-  console.log("API running on http://localhost:3000")
-);
-function toTable(players) {
-  return {
-    columns: ["Nama", "Posisi", "Club", "Liga", "Negara", "Instruction"],
-    rows: players.map(p => [
-      p.NamaPemain,
-      p.Posisi,
-      p.Club,
-      p.Liga,
-      p.Negara,
-      p.PlayerInstruction
-    ])
-  };
-}
-
-//Terminal Version
-// async function main() {
-//   const rl = readline.createInterface({ input, output });
-
-//   console.log("Pilih Formasi:");
-//   console.log("433 → 4-3-3 (Tiki-Taka)");
-//   console.log("4312 → 4-3-1-2 (Kick and Rush)");
-//   console.log("4222 → 4-2-2-2 (Gegen Pressing)");
-//   console.log("4213 → 4-2-1-3 (Jogo Bonito)");
-
-//   const f = await rl.question("Masukkan formasi: ");
-//   const formation = formations[f.trim()];
-//   const counterPlayer = await rl.question("Jumlah Pemain per Posisi (Minimal 6): ");
+// app.post("/api/optimize-squad", (req, res) => {
+//     const { formation: key,jumlahPemainPerPosisi:countPlayer} = req.body;
+//     const formation = formations[key];
 
 //   if (!formation) {
-//     console.log("Formasi tidak valid");
-//     rl.close();
-//     return;
+//     return res.status(400).json({
+//       error: "Formasi tidak valid"
+//     });
 //   }
 
-//   console.log("\nFormasi:", formation.name);
-//   console.log("Taktik:", formation.tactic);
+//   const preselected = preselectPlayers(playersByPosition,formation.tactic,countPlayer);
 
-//   const preselected = preselectPlayers(playersByPosition,formation.tactic,counterPlayer);
-//   console.log("\nPlayers List:");
-//   const tableData = Object.values(preselected)
-//   .flat()
-//   .map(p => ({
-//     Nama: p.NamaPemain,
-//     Posisi: p.Posisi,
-//     Club: p.Club,
-//     Instruction: p.PlayerInstruction
-//   }))
-//   console.table(tableData);
+//   // ===== ITERATIVE  =====
+// const iterStart = performance.now();
+// const itResult = backtrackingIterative(
+//   formation.positions,
+//   preselected
+// );
+// const iterEnd = performance.now();
+
+// // ===== RECURSIVE =====
+// const recStart = performance.now();
+// const recResult = backtrackingRecursive(
+//   formation.positions,
+//   preselected
+// );
+// const recEnd = performance.now();
+
+//   res.json({
+//     formation: key,
+//     jumlahPemainPerPosisi: countPlayer,
+//     runtime: {
+//     iterativeMs: Number((iterEnd - iterStart).toFixed(2)),
+//     recursiveMs: Number((recEnd - recStart).toFixed(2))
+//     },
+//     preselectPlayers: {
+//       total: Object.values(preselected).flat().length,
+//       table: toTable(Object.values(preselected).flat())
+//     },
+//     iterativeResult: {
+//       chemistry: itResult.score,
+//       table: toTable(itResult.squad)
+//     },
+//     recursiveResult: {
+//       chemistry: recResult.score,
+//       table: toTable(recResult.squad)
+//     }
+//   });
+// })
+// app.listen(3000, () =>
+//   console.log("API running on http://localhost:3000")
+// );
+// function toTable(players) {
+//   return {
+//     columns: ["Nama", "Posisi", "Club", "Liga", "Negara", "Instruction"],
+//     rows: players.map(p => [
+//       p.NamaPemain,
+//       p.Posisi,
+//       p.Club,
+//       p.Liga,
+//       p.Negara,
+//       p.PlayerInstruction
+//     ])
+//   };
+// }
+
+async function main() {
+  const rl = readline.createInterface({ input, output });
+
+  // ✅ input N dulu (minimal 5)
+  const nInput = await rl.question("Masukkan jumlah kandidat per posisi (minimal 5): ");
+  const n = Number.parseInt(nInput.trim(), 10);
+
+  if (!Number.isFinite(n) || n < 5) {
+    console.log("Input tidak valid. Minimal 5.");
+    rl.close();
+    return;
+  }
+
+  console.log("Pilih Formasi:");
+  console.log("433 → 4-3-3 (Tiki-Taka)");
+  console.log("4312 → 4-3-1-2 (Kick and Rush)");
+  console.log("4222 → 4-2-2-2 (Gegen Pressing)");
+  console.log("4213 → 4-2-2-2 (Jogo Bonito)");
+
+  const f = await rl.question("Masukkan formasi: ");
+  const formation = formations[f.trim()];
+
+  if (!formation) {
+    console.log("Formasi tidak valid");
+    rl.close();
+    return;
+  }
+
+  console.log("\nFormasi:", formation.name);
+  console.log("Taktik:", formation.tactic);
+
+  const preselected = preselectPlayers(playersByPosition, formation.tactic, n);
+
+  console.log("\nPlayers List:");
+  const tableData = Object.values(preselected)
+  .flat()
+  .map(p => ({
+    Nama: p.NamaPemain,
+    Posisi: p.Posisi,
+    Club: p.Club,
+    Instruction: p.PlayerInstruction
+  }))
+  console.table(tableData);
 
 
-//  console.log("\n=== HASIL RUNNING TIME ===");
-//  console.time("Backtracking Recursive");
-//   const recResult = backtrackingRecursive(
-//     formation.positions,
-//     preselected
-//   );
-//   console.timeEnd("Backtracking Recursive");
+ console.log("\n=== HASIL RUNNING TIME ===");
+ console.time("Backtracking Recursive");
+  const recResult = backtrackingRecursive(
+    formation.positions,
+    preselected
+  );
+  console.timeEnd("Backtracking Recursive");
 
-//   console.time("Backtracking Iterative");
-//   const itResult = backtrackingIterative(
-//     formation.positions,
-//     preselected
-//   );
-//   console.timeEnd("Backtracking Iterative");
+  console.time("Backtracking Iterative");
+  const itResult = backtrackingIterative(
+    formation.positions,
+    preselected
+  );
+  console.timeEnd("Backtracking Iterative");
 
   
-//   console.log("\n=== HASIL ===");
-//   console.log("Recursive Chemistry:", recResult.score);
-//   console.log("Iterative Chemistry:", itResult.score);
+  console.log("\n=== HASIL ===");
+  console.log("Recursive Chemistry:", recResult.score);
+  console.log("Iterative Chemistry:", itResult.score);
 
-//   console.table(itResult.squad.map(p => ({
-//     Nama: p.NamaPemain,
-//     Posisi: p.Posisi,
-//     Club: p.Club,
-//     Negara: p.Negara,
-//     Instruction: p.PlayerInstruction
-//   })));
+  console.table(itResult.squad.map(p => ({
+    Nama: p.NamaPemain,
+    Posisi: p.Posisi,
+    Club: p.Club,
+    Negara: p.Negara,
+    Instruction: p.PlayerInstruction
+  })));
 
-//   rl.close();
-// }
-// main();
+  rl.close();
+}
+
+main();
 
